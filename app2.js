baf75e0adf40bf6b4f53b636068b72c3272887bc
Kimi Sian-yu Chen <sianyu@icloud.com>
2018-09-10 02:29:18 +0800

    update

--- app2.js
+++ app2.js
@@ -66,7 +66,7 @@ let mongo_query = function(db, para) {
         });
 
         function simplePipeline(db, callback) {
-            const collection = db.collection('site90');
+            const collection = db.collection('site92');
             collection.aggregate(
                 [
                     {
@@ -100,53 +100,210 @@ app.get('/', async (req, res, next) => {
 
     let ETC_price_data;
 
-    await rp('https://poloniex.com/public?command=returnChartData&currencyPair=USDT_ETC&start=1509508800&end=9999999999&period=900')
-        .then(function (htmlString) {
-            // Process html...
-            ETC_price_data = JSON.parse(htmlString);
-            //console.log(ETC_price_data);
-        })
-        .catch(function (err) {
-            // Crawling failed...
-        });
-
-
-    client = await MongoClient.connect(url);
+    client = await MongoClient.connect(url,{ useNewUrlParser: true });
     console.log("Connected correctly to server");
     const db = client.db(dbName);
     // Get the collection
-    //const col = db.collection('site89');
-    //const docs = await col.find().limit(5).toArray();
+    const col = db.collection('site96');
+    const docs = await col.find().limit(50000000000).toArray();
+    //const docs = await col.find( { value: { $gte: 50000 } } ).toArray();
+    //console.log(docs.length);
+    //console.log(docs);
 
+    //Find max_token_transfer
+    let max_token_transfer = await Promise.all([_.maxBy(docs, function(o) {
+        return o.value;
+    })]);
+    console.log("max_token_transfered value");
+    console.log(max_token_transfer);
 
-    let etc_token_hour = await mongo_query(db, 11);
+    let max_token_flow_tx = await Promise.all([_.find(docs, function(o) {
+        return "0x08a98920ec43025440e8cc4418967e0a681caf8ee064549397481f4849148124" == o._id;
+    })]);
 
-    //console.log("111"+etc_token_hour);
+    let address=[];
 
-    let array_etc_token_hour=[];
-    for (let i=0;i<etc_token_hour.length;i++)
-    {
-        //console.log(1);
-        console.log(etc_token_hour[i].totalAmount);
-        console.log(etc_token_hour[i]._id);
-        console.log(moment().year(etc_token_hour[i]._id.year).month(etc_token_hour[i]._id.month-1).date(etc_token_hour[i]._id.day).hour(etc_token_hour[i]._id.hour));
-        let obj1={
-            "date":moment().year(etc_token_hour[i]._id.year).month(etc_token_hour[i]._id.month-1).date(etc_token_hour[i]._id.day).hour(etc_token_hour[i]._id.hour).add(7, 'h'),
-            "value":etc_token_hour[i].totalAmount
-        };
-        array_etc_token_hour.push(obj1);
+    let obj2={
+        "address" : max_token_flow_tx[0].from
+    }
+    let obj3={
+        "address" : max_token_flow_tx[0].to
     }
 
+    address.push(obj2)
+    address.push(obj3)
+
+    //console.log(max_token_flow_tx[0].timestamp_unix);
+    let find_layer2_node = await Promise.all([_.filter(docs, function(o) {
+        return max_token_flow_tx[0].to == o.from ;
+    })]);
+
+    //console.log(find_layer2_node[0]);
+
+//remove node2 is early than Node1
+    let layer2_node_filterd_early_time=[]
+    await Promise.all([
+        _.forEach(find_layer2_node[0], function(o) {
+            //console.log("Node 1 Time : ")
+            //console.log(max_token_flow_tx[0].timestamp_unix);
+            //console.log("Node 2 Time : ")
+            //console.log(o.timestamp_unix)
+
+            if (o.timestamp_unix > max_token_flow_tx[0].timestamp_unix){
+                //console.log("Node2 time > Node1 time");
+                layer2_node_filterd_early_time.push(o);
+            }
+            else {
+                //console.log("Failed, Node1 time > Node2 time");
+            }
+
+
+        })
+    ]);
 
 
 
+
+
+    let sort_layer2_node = await Promise.all([
+        //_.sortBy(find_layer2_node[0], [function(o) {
+        _.sortBy(layer2_node_filterd_early_time, [function(o) {
+            //console.log(o.value);
+            return o.value; }])
+    ]);
+    //console.log(sort_layer2_node);
+
+    let sort_layer2_node_take_right = await Promise.all([
+        _.takeRight(sort_layer2_node[0], 1000000000000000000)
+    ]);
+
+    let sort_layer2_node_take_right_max_value = await Promise.all([
+        _.maxBy(sort_layer2_node_take_right[0], function(o) { return o.value; })
+    ]);
+
+    //console.log(sort_layer2_node_take_right_max_value);
+
+
+    //console.log(sort_layer2_node_take_right[0])
+
+    await Promise.all([
+        _.forEach(sort_layer2_node_take_right[0], function(value) {
+            //console.log(value);
+            let obj1={
+                "address" : value.to
+            }
+            address.push(obj1);
+        })
+    ]);
+
+
+    //layer3
+    let layer3_array=[];
+    for (let i=0;i<sort_layer2_node_take_right[0].length;i++){
+        //console.log("------------");
+        //console.log("Node "+i)
+        //console.log(sort_layer2_node_take_right[0][i]);
+
+
+        //console.log("i: "+i);
+        let find_layer3_node = await Promise.all([_.filter(docs, function(o) {
+            return sort_layer2_node_take_right[0][i].to == o.from ;
+        })]);
+
+        //console.log("Sub Nodes"+find_layer3_node);
+        //console.log(find_layer3_node);
+        //layer3_array.push(find_layer3_node[0]);
+        //console.log(find_layer2_node[0].length);
+
+        for(let j=0;j<find_layer3_node[0].length;j++)
+        {
+            //console.log("j: "+j);
+            //console.log("Layer2 TImestamp : "+sort_layer2_node_take_right[0][i].timestamp_unix);
+            //console.log("Layer3 TImestamp : "+find_layer3_node[0][j].timestamp_unix)
+            if (sort_layer2_node_take_right[0][i].timestamp_unix < find_layer3_node[0][j].timestamp_unix){
+                //console.log("Node3 is behind Node2, OK");
+                //console.log("Node3 add new field, parent timestamp");
+                find_layer3_node[0][j].parent_timestamp=sort_layer2_node_take_right[0][i].timestamp_unix;
+                //console.log("Push to layer3_arrray");
+                layer3_array.push(find_layer3_node[0][j]);
+
+
+                //console.log("Push layer3 address to address");
+                //console.log(find_layer3_node[0][j].to)
+                //address.push(find_layer3_node[0][j].to)
+                let obj1={
+                    "address" : find_layer3_node[0][j].to
+                };
+                address.push(obj1);
+            }
+            else{
+                //console.log("Node3 is ahead of  Node2, Failed");
+            }
+        }
+
+
+
+    }
+
+    //console.log("node3_array");
+    //console.log(layer3_array);
+
+    //node4
+    let node4_array=[];
+    for (let i=0;i<layer3_array.length;i++){
+        //console.log("------------");
+        //console.log("Node4 No."+i)
+        //console.log(layer3_array[i]);
+
+        let find_Node4 = await Promise.all([_.filter(docs, function(o) {
+            return layer3_array[i].to == o.from ;
+        })]);
+        //console.log("Finded Node4")
+        //console.log(find_Node4[0])
+
+        for(let j=0;j<find_Node4[0].length;j++)
+        {
+            //console.log("j: "+j);
+            //console.log("Node3 TImestamp : "+layer3_array[i].timestamp_unix);
+            //console.log("Node4 TImestamp : "+find_Node4[0][j].timestamp_unix)
+
+            if (layer3_array[i].timestamp_unix < find_Node4[0][j].timestamp_unix){
+                //console.log("Node4 is behind Node3, OK");
+                //Add Node3 time to Node4 obj
+                find_Node4[0][j].parent_timestamp=layer3_array[i].timestamp_unix;
+                //Push Node4[j] to Node4_array
+                node4_array.push(find_Node4[0][j]);
+
+                //"Push Node4 address to address
+                let obj1={
+                    "address" : find_Node4[0][j].to
+                };
+                address.push(obj1);
+
+            }
+            else{
+                //console.log("Node4 is ahead of Node3, Failed");
+            }
+
+        }
+
+    }
+
+    console.log("All Address");
+    console.log(address.length);
 
     let data = {
         //"sender": trans,
+        "max_token_flow_tx":max_token_flow_tx[0],
         //"sender2": trans2,
-        //"address": address,
-        "array_etc_token_hour": array_etc_token_hour,
-        "ETC_price_data": ETC_price_data,
+        "address": address,
+        "sort_layer2_node_take_right":sort_layer2_node_take_right[0],
+        "sort_layer2_node_take_right_max_value":sort_layer2_node_take_right_max_value,
+        //"find_layer3_node":find_layer3_node[0],
+        "layer3_array": layer3_array,
+        "node4_array": node4_array,
+        //"array_etc_token_hour": array_etc_token_hour,
+        //"ETC_price_data": ETC_price_data,
         //"hash_min": hash_min,
         //"hash_latest": hash_latest,
         //"ETC_price_data": ETC_price_data,
--- public/1041.js
+++ public/1041.js
@@ -1,13 +1,24 @@
 // set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
+var margin = {top: 20, right: 20, bottom: 30, left: 270},
     width = 1300 - margin.left - margin.right,
-    height = 650 - margin.top - margin.bottom;
+    height = 750 - margin.top - margin.bottom;
 
-//var x = d3.scaleLinear().range([0, 50]);
+
 var x = d3.scaleTime().range([0, width]);
-//var y = d3.scaleTime().range([0, height]);
-var y = d3.scaleLinear().range([height, 0]);
-var y2 = d3.scaleLinear().range([height, 0]);
+
+
+var y = d3.scaleBand()
+    //.rangeRound([0, height])
+    .range([0, height])
+    .padding(1);
+
+//onsole.log(data_array.max_token_flow_tx);
+y.domain(data_array.address.map(function(d) {
+    //console.log(d);
+    return d.address;
+}));
+
+
 
 var _s1 = d3.format("s");
 
@@ -93,11 +104,10 @@ var jsonCircles = [
 // Scale the range of the data
 //x.domain([0, 1]);
 //x.domain(d3.extent(jsonCircles, function(d) { return new Date(d.Milestone); }));
-x.domain([new Date("2018-08-31"), new Date("2018-9-6")]);
+x.domain([new Date("2018-09-2"), new Date("2018-9-11")]);
 //x.domain([new Date("2018-07-15"), new Date("2018-9-10")]);
 //y.domain(d3.extent(jsonCircles, function(d) { return new Date(d.Milestone); }));
-y.domain([0, 500000]);
-y2.domain([6, 20]);
+//y.domain([0, 500000]);
 
 svg.append("g")
     .attr("transform", "translate(0," + height + ")")
@@ -107,45 +117,13 @@ svg.append("g")
 // Add the Y Axis
 svg.append("g")
     .attr("stroke", "green")
-    .call(d3.axisLeft(y)
-        .tickFormat(d3.format(".2s")));
-
-//add 2nd Y Axis
-svg.append("g")
-    .attr("class", "y axis")
-    .attr("transform", "translate(" + width + " ,0)")
-    //.style("fill", "red")
-    .attr("stroke", "purple")
-    .call(d3.axisLeft(y2));
+    .call(d3.axisLeft(y));
+//.tickFormat(d3.format(".2s")));
 
 
-console.log(data_array.array_etc_token_hour);
-
-var circles5 = svg.selectAll("circle5")
-    .data(data_array.array_etc_token_hour)
-    .enter()
-    .append("circle")
-    .attr("clip-path","url(#rect-clip)")
-    .attr("cx", function (d) {
-        //console.log(d.timestamp);
-        return x(new Date(d.date)); })
-    .attr("cy", function (d) {
-        //console.log(d);
-        return y(d.value);
-        //return y(d.totalDifficulty);
-    })
-    .attr("r", function (d) { return 5; })
-    .attr('fill-opacity', function(d) {
-        if (d.status == "completed"){
-            return 1;
-        }
-        else {
-            return 0.5;
-        }
-    })
-    .style("fill", "green");
 
 //add ETC price data
+/*
 let circles6 = svg.selectAll("circle6")
     .data(data_array.ETC_price_data)
     .enter()
@@ -169,21 +147,109 @@ let circles6 = svg.selectAll("circle6")
         }
     })
     .style("fill", "purple");
+    */
 
-svg.append("text")
-    .attr("x", x(new Date())-800)
-    .attr("y", -10)
-    .style("font-size", "14px")
-    .attr("transform", "translate(0,0) rotate(90)")
-    .attr('text-anchor', 'middle')
-    .attr("stroke", "green")
-    .text("$ETC Token Movement per hour");
+//console.log(data_array.sort_layer2_node_take_right)
 
-svg.append("text")
-    .attr("x", x(new Date())-800)
-    .attr("y", -1180)
-    .style("font-size", "14px")
-    .attr("transform", "translate(0,0) rotate(90)")
-    .attr('text-anchor', 'middle')
-    .attr("stroke", "purple")
-    .text("$ETC/USD");
+
+let linearScale1 = d3.scaleLinear()
+    //.domain([0,3500])
+    .domain([0,80000])
+    .range([0,1]);
+//console.log(data_array.sort_layer2_node_take_right_max_value[0].value);
+//console.log(linearScale1(50));
+
+
+let line1 = svg.selectAll("line1")
+    .data(data_array.sort_layer2_node_take_right)
+    .enter()
+    .append("line")
+    .attr("clip-path","url(#rect-clip)")
+    .attr("x1", function (d) {
+        //console.log(d);
+        return 50;
+    })
+    .attr("y1", function (d) {
+        //console.log(d.from);
+        return y(d.from);
+        //return y(d.totalDifficulty);
+    })
+    .attr("x2", function (d) {
+        //console.log(new Date(d.timestamp_unix*1000));
+        return x(new Date(d.timestamp_unix*1000));
+        //return y(d.totalDifficulty);
+    })
+    .attr("y2", function (d) {
+
+        //console.log(d);
+        return y(d.to);
+        //return y(d.totalDifficulty);
+    })
+    .style("opacity", function (d) {
+        return linearScale1(d.value);
+    })
+    .attr("stroke-width", 1)
+    .attr("stroke","red");
+
+
+let line2 = svg.selectAll("line2")
+    .data(data_array.layer3_array)
+    .enter()
+    .append("line")
+    .attr("clip-path","url(#rect-clip)")
+    .attr("x1", function (d) {
+        //console.log(d);
+        return x(new Date(d.parent_timestamp*1000));
+    })
+    .attr("y1", function (d) {
+        //console.log(d.from);
+        return y(d.from);
+        //return y(d.totalDifficulty);
+    })
+    .attr("x2", function (d) {
+        //console.log(new Date(d.timestamp_unix*1000));
+        return x(new Date(d.timestamp_unix*1000));
+        //return y(d.totalDifficulty);
+    })
+    .attr("y2", function (d) {
+
+        //console.log(d);
+        return y(d.to);
+        //return y(d.totalDifficulty);
+    })
+    .style("opacity", function (d) {
+        return linearScale1(d.value);
+    })
+    .attr("stroke-width", 1)
+    .attr("stroke","black");
+
+let line4 = svg.selectAll("line4")
+    .data(data_array.node4_array)
+    .enter()
+    .append("line")
+    .attr("clip-path","url(#rect-clip)")
+    .attr("x1", function (d) {
+        //console.log(d);
+        return x(new Date(d.parent_timestamp*1000));
+    })
+    .attr("y1", function (d) {
+        //console.log(d.from);
+        return y(d.from);
+        //return y(d.totalDifficulty);
+    })
+    .attr("x2", function (d) {
+        //console.log(new Date(d.timestamp_unix*1000));
+        return x(new Date(d.timestamp_unix*1000));
+        //return y(d.totalDifficulty);
+    })
+    .attr("y2", function (d) {
+
+        //console.log(d);
+        return y(d.to);
+        //return y(d.totalDifficulty);
+    })
+    .style("opacity", function (d) {
+        return linearScale1(d.value);
+    })
+    .attr("stroke-width", 1)
+    .attr("stroke","blue");
